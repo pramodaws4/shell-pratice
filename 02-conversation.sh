b636da0abd028fc084cb4132029dd83449ce46d9
pramod kumar m <pramodaws4@gmail.com>
2026-01-30 23:37:17 +0530

    commiting the change

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "ramesh :: hai suresh how are you?"
+echo "suresh :: hi ramesh I am fine. how about you?"
+echo "ramesh :: I am also good. what are you doing these days?"
+echo "suresh :: I am learning shell scripting. it's really interesting."
+echo "ramesh :: that's great! shell scripting is very useful for automating tasks."
