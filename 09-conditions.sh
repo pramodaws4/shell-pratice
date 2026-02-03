ee5fd6a2fc9e81c0423ee6710b0f536212423536
pramod kumar m <pramodaws4@gmail.com>
2026-02-03 18:37:31 +0530

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NUMBAR=1
+#conditions in shell scripting
+# if condition
+  # -gt : greater than
+  # -lt : less than
+  # -eq : equal to
+  # -ne : not equal to
+  # -ge : greater than or equal to
+  # -le : less than or equal to
+if [$NUMBER -eq 1]; then
+  echo "given number $NUMBER is grater than 20"
+elif [$NUMBER -eq 20]; then
+  echo "given number $NUMBER is equal to 20"
+else
+  echo "given number $NUMBER is less than 20"
+fi
