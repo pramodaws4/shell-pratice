849f18765d2aa30b0dc43a9d7c55097b9e2cc196
pramod kumar m <pramodaws4@gmail.com>
2026-02-03 18:11:52 +0530

    actions

---
+++ 08-datatypes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUM1=200
+NUM2=50
+SUM=$(($NUM1 + $NUM2))
+echo "Sum of $NUM1 and $NUM2 is: $SUM"
+
+
+num3=15
+num4=4
+Sum=$(($num3 + $num4))
+echo "Sum of $num3 and $num4 is: $Sum"  
+
