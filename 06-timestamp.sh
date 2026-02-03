7454d817f3b2ad434ff8dc2a0814fee3c45d3874
pramod kumar m <pramodaws4@gmail.com>
2026-02-03 17:51:57 +0530

    actions

--- 06-timestamp.sh
+++ 06-timestamp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+TIMESTAMP=$(date +%s)
 echo "Current date and time: $TIMESTAMP"
 
 sleep 15
 
-END_TIME=$(date date +%s)
-echo "This script was executed on: $END_TIME"
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $TIMESTAMP))
+echo "Total time elapsed: $TOTAL_TIME seconds"
+
+echo "This script was executed on: $END_TIME "
 
