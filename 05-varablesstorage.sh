d324c2a242cd52cf643eda3b58bfe8d45bc66aed
pramod kumar m <pramodaws4@gmail.com>
2026-02-03 17:09:47 +0530

    actions

--- 05-varablesstorage.sh
+++ 05-varablesstorage.sh
@@ -7,3 +7,4 @@ echo "Hello, $user_name! Welcome to the shell scripting world."
 
 echo "please enter your password: "
 read -s user_password
+echo "Thank you! Your password has been securely received. your password is $user_password"
