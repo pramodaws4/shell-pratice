f333dffc421e5ebc07c27845e46907b818c8754d
pramod kumar m <pramodaws4@gmail.com>
2026-02-03 16:32:48 +0530

    actions

---
+++ 03-varables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ramesh
+PERSON2=Suresh
+echo "$PERSON1 :: hai $PERSON2 how are you?"
+echo "$PERSON2 :: hi $PERSON1 I am fine. how about you?"
+echo "$PERSON1 :: I am also good. what are you doing these days?"
+echo "$PERSON2 :: I am learning shell scripting. it's really interesting."
+echo "$PERSON1 :: that's great! shell scripting is very useful for automating tasks."
