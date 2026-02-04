86f92d1818b070636831128d1809da8184d39d86
pramod kumar m <pramodaws4@gmail.com>
2026-02-04 08:25:06 +0530

    actions

---
+++ 16-colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##colors
+R='\e[31m' #red
+G='\e[32m' #green
+Y='\e[33m' #yellow
+B='\e[34m' ##blue
+D='\e[0m' ##default color
+
+echo -e "$R iam uarning $b iam luarning linux shell scripting $D"
