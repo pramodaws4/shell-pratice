03e1a59ca4cfcd14dda3663ed603a6311a478c3c
pramod kumar m <pramodaws4@gmail.com>
2026-02-04 08:11:27 +0530

    actions

---
+++ 15-loosinstalled.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/ver/log/shell-script"
+LOG_FILE="/ver/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+echo "Please run this root user access." | tee -a $LOG_FILE
+fi
+
+mkdir -p $LOG_FOLDER
+
+validate(){
+if [ $1 -ne 0 ]; then 
+    echo "$2 installation ... failed" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "$2 installation .... successfully" | tee -a $LOG_FILE
+fi
+}
+
+for package in $@
+do
+    dnf install $package -y    &>> $LOG_FILE
+    if [$? -ne 0 ]; then
+        echo "$package not installed .. installing now" | tee -a $LOG_FILE
+        dnf  install $package -y    &>> $LOG_FILE
+        validate $? "$package installing"
+    else
+        echo "$package installation already done skipping"| tee -a $LOG_FILEILE
+    fi
+
+done
