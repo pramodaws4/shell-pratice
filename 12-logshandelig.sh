122776f53c58a52ad1426b2af64ecb7b4d466b0a
pramod kumar m <pramodaws4@gmail.com>
2026-02-04 07:53:24 +0530

    actions

--- 12-logshandelig.sh
+++ 12-logshandelig.sh
@@ -6,17 +6,17 @@ LOG_FILE="/ver/log/shell-script/$0.log"
 
 
 if [ $USERID -ne 0 ]; then
-echo "Please run this root user access."
+echo "Please run this root user access." | tee -a $LOG_FILE
 fi
 
 mkdir -p $LOG_FOLDER
 
 validate(){
 if [ $1 -ne 0 ]; then 
-    echo "$2 installation ... failed"
+    echo "$2 installation ... failed" | tee -a $LOG_FILE
     exit 1
 else
-    echo "$2 installation .... successfully"
+    echo "$2 installation .... successfully" | tee -a $LOG_FILE
 fi
 }
 
---
+++ 13-loops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##loops in shell scripting
+
+for i in {1..5}
+do 
+echo "printing the loop value "$i
+done
